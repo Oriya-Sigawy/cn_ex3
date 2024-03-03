90ef475f7f4b512a5ba403e04014af9e04e33815
Oriya-Sigawy <148494511+Oriya-Sigawy@users.noreply.github.com>
2024-03-03 19:01:21 +0200

    update the time calculates and the prints

---
+++ Makefile
@@ -0,0 +1,14 @@
+CFLAGS= -Wall -g
+
+.PHONY: all clean
+
+all: TCP_Receiver TCP_Sender
+
+TCP_Receiver: TCP_Receiver.c
+	gcc -o TCP_Receiver TCP_Receiver.c
+
+TCP_Sender: TCP_Sender.c
+	gcc -o TCP_Sender TCP_Sender.c
+
+clean:
+	rm -f *.o TCP_Receiver TCP_Sender
--- TCP_Receiver.c
+++ TCP_Receiver.c
@@ -5,172 +5,59 @@
 #include <string.h>
 #include <sys/types.h>
 #include <netinet/in.h>
-#include <netinet/tcp.h>        
+#include <netinet/tcp.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <signal.h>
 #include <time.h>
 #include <sys/time.h>
-#include "sock.h"
 
-char * reno= "reno";
-char * cubic= "cubic";
-bool algoritm;
+#define RECEIVER_IP_ADDRESS "127.0.0.1"
+#define RECEIVER_PORT 8888 // TODO receive this details from user
 
-int main(int argc, char *argv[])
+char *reno = "reno";
+char *cubic = "cubic";
+char *algoritm;
+
+typedef struct
 {
-    algoritm= argv[2];
+    double *data;
+    int size;
+    int capacity;
+} DynamicArray;
 
-    struct sockaddr_in receiverAddr, senderAddr;
-    char senderAddrArr[INET_ADDRSTRLEN];
-    socklen_t senderAddrLen;
-
-    char* buff = NULL;
-    int fSize;
-    int received = 0;
-
-    int numOfSends = 0; 
-
-    int my_sock = -1, sender_sock = -1;
-   
-    struct timeval start, end;
-    double *times = (double*) malloc(10 * sizeof(double));              //array for the time calculating, every cell for one send of the information    
-
-    int can_be_reused=1;
-    printf("Starting Receiver...\n");
-    if ((my_sock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+void AddToDArr(DynamicArray *arr, double value)
+{
+    if (arr->size >= arr->capacity)
     {
-        printf("couldn't create socket");
-        exit(1);
+        arr->capacity *= 2;
+        arr->data = (double *)realloc(arr->data, arr->capacity * sizeof(double));
     }
-    memset(&receiverAddr, 0, sizeof(receiverAddr));
-    receiverAddr.sin_family = AF_INET;
-    receiverAddr.sin_addr.s_addr = INADDR_ANY;
-    receiverAddr.sin_port = htons(RECEIVER_PORT);
-
-    if (setsockopt(my_sock, SOL_SOCKET, SO_REUSEADDR, &can_be_reused, sizeof(can_be_reused)) == -1)
+    arr->data[arr->size] = value;
+    arr->size++;
+}
+void free_arr(DynamicArray *arr)
+{
+    for (size_t i = 0; i < arr->size; i++)
     {
-        printf("setsockopt failed\n");
-        exit(1);
+        free(arr->data);
     }
-
-    if (bind(my_sock, (struct sockaddr *) &receiverAddr, sizeof(&receiverAddr)) == -1)
-    {
-        printf("bind failed\n");
-        exit(1);
-    }
-
-    if (listen(my_sock, MAX_SENDERS) == -1)
-    {
-        printf("listen() failed\n");  
-        exit(1);
-    }
-
-    renoOrCubic(my_sock, algoritm);
-
-    printf("Waiting for TCP connection...\n");
-    memset(&senderAddr, 0, sizeof(senderAddr));
-
-    senderAddrLen = sizeof(senderAddr);
-    sender_sock = accept(my_sock, (struct sockaddr *) &senderAddr, &senderAddrLen);
-
-     if (sender_sock == -1)
-    {
-        printf("didn't accept the socket\n");
-        exit(1);
-    }
-        
-    inet_ntop(AF_INET, &(senderAddr.sin_addr), senderAddrArr, INET_ADDRSTRLEN);
-
-    printf("Sender connected, receiving the file's size...\n");
-    getInformation(sender_sock, &fSize, sizeof(int));
-    adjust(sender_sock, true);
-    printf("received size successfuly\n");
-
-    buff = malloc(fSize * sizeof(char));
-
-    if (buff == NULL)
-    {
-        printf("malloc failed\n");
-        exit(1);
-    }
-
-    printf("beginning to receive files...\n");
-
-    int temp=5;
-    while (true)
-    {
-        int receivedPart;
-        if (!received)
-        {
-            memset(buff, 0, fSize);
-            gettimeofday(&start, NULL);
-            
-        }
-
-        receivedPart = getInformation(sender_sock, buff + received, fSize - received);
-
-        if (!receivedPart)
-        {
-            break;
-        }
-        
-        received+=receivedPart;
-
-        if(received==fSize)     //received the entire file
-        {
-
-        gettimeofday(&end, NULL);
-        times[numOfSends]= ((end.tv_sec - start.tv_sec)*1000) + (((double)(end.tv_usec - start.tv_usec))/1000);
-        numOfSends++;
-
-        if (numOfSends >= temp)     //more memory needs to be allocate cause the num of sends is bigger than what we already allocated
-            {
-                temp += 5;
-                double* p = (double*) realloc(times, (temp * sizeof(double)));
-                times=p;
-            }
-
-        }
-
-        printf("File transfer complete\n");
-
-        if (!adjust(sender_sock, false))        //check the user's desicion whether the sender will send the file again or not
-        {
-                    break;
-        }
-
-        adjust(sender_sock, true);
-        received=0;
-    }
-    close(sender_sock);
-    printf("Sender sent exit message\n");
-
-    timeCalc(times, numOfSends, fSize);
-    usleep(1000);
-
-    close(my_sock);                         //receiver socket closed
-
-    free(times);                            //free memory
-    free(buff);
-    
-    return 0;
-}   
-
+    free(arr);
+}
 /**
- *  This func gets the information from the sender. 
+ *  This func gets the information from the sender.
  *  sender_sock: the sender's socket descriptor (representing the socket's endpoint)
  *  buf: will contain the accapted information
  *  len: length of the information
-*/
+ */
 int getInformation(int sender_sock, void *buff, int len)
 {
     int receive = recv(sender_sock, buff, len, 0);
 
     if (receive == -1)
     {
-        printf("receive failed");
+        perror("receive");
         exit(1);
     }
 
@@ -183,48 +70,17 @@ int getInformation(int sender_sock, void *buff, int len)
     return receive;
 }
 
-
 /**
- *  This func sends the information to the sender. 
- *  sender_sock: the sender's socket descriptor (representing the socket's endpoint)
- *  buf: contains the information to send
- *  len: length of the information to send
- *  returns: the num of bytes who succesfuly sent.
-*/
-int sendInformation(int sender_sock, void* buf, int len)
+ *   This func changes the TCP Congestion control algoritm.
+ *   my_sock: the receiver's socket descriptor (representing the socket's endpoint)
+ *   rOc: algoritm to switch to, 1 for RENO, 0 for CUBIC
+ */
+void renoOrCubic(int my_sock, char *rOc)
 {
-    int sentInf = send(sender_sock, buf, len, 0);
-
-    if (sentInf == -1)
+    if (rOc)
     {
-        printf("ERROR! send failed\n");
-        exit(1);
-    }
-
-    else if (!sentInf)
-        printf("ERROR! Receiver didn't accept my request\n");
-
-    else if (sentInf < len)
-        printf("ERROR! send only part of the information\n");
-
-    else
-        printf("send success");
-
-    return sentInf;
-}
-
-
-/**
-*   This func changes the TCP Congestion control algoritm.
-*   my_sock: the receiver's socket descriptor (representing the socket's endpoint)
-*   rOc: algoritm to switch to, 1 for RENO, 0 for CUBIC
-*/
- void renoOrCubic(int my_sock, int rOc)
-{
-    if(rOc)
-    {
-        socklen_t renoLen=strlen(reno);
-        if(setsockopt(my_sock, IPPROTO_TCP, TCP_CONGESTION, reno, renoLen)!=0)
+        socklen_t renoLen = strlen(reno);
+        if (setsockopt(my_sock, IPPROTO_TCP, TCP_CONGESTION, reno, renoLen) != 0)
         {
             perror("setsockopt");
             exit(1);
@@ -232,52 +88,13 @@ int sendInformation(int sender_sock, void* buf, int len)
     }
     else
     {
-        socklen_t cubicLen=strlen(cubic);
-        if(setsockopt(my_sock, IPPROTO_TCP, TCP_CONGESTION, cubic, cubicLen)!=0)
+        socklen_t cubicLen = strlen(cubic);
+        if (setsockopt(my_sock, IPPROTO_TCP, TCP_CONGESTION, cubic, cubicLen) != 0)
         {
             perror("setsockopt");
             exit(1);
         }
     }
-}
-
-/**
- *  this func has 2 uses:
- *  1. this func helps to adjust the times between the server and the receiver and make a correct calculation of time.
- *  2. this func sends a confirmation when all the segments of the information accapted.
- *  my_sock: the receiver's socket descriptor (representing the socket's endpoint)
- *  loss: 1 if some of the packets got lost in their way, 0 if not
- * returns: 1 for succesfuly send, 0 if the socket was closed, exit(1) if were a problem in the sending.
-*/
-int adjust(int my_sock, bool loss)
-{
-    char c = 0;
-    int x = 0;
-
-    if (loss)
-    {
-        x = send(my_sock, &c, sizeof(char), 0);
-
-        if (x == -1)
-        {
-            perror("send");
-            exit(1);
-        }
-
-        return x;
-    }
-
-   printf("Waiting for sender response...");
-    x = recv(my_sock, &c, sizeof(char), 0);
-
-    if (x == -1)
-    {
-        perror("recv error");
-        exit(1);
-    }
-
-
-    return x;
 }
 
 /**
@@ -285,23 +102,157 @@ int adjust(int my_sock, bool loss)
  * times: array of the saved times, one cell for every time that the sender sended the information.
  * numOfSends: the number of sends that the senders sended the information
  * fSize: the size of the sended file
-*/
-void timeCalc(double* times, int numOfSends, int fSize) {
+ */
+void print_times(DynamicArray *arr, double fSize)
+{
     double sum = 0;
-    // double avg;
-    // double rate;
-
-    for (int i = 0; i < numOfSends; ++i)
+    double avg = 0;
+    double bitrate;
+    printf("* statistics: *\n");
+    for (size_t i = 0; i < arr->size; i++)
     {
-        sum += times[i];
-        printf("Run # %d Data: Time= %0.3lf ms; Speed=\n", (i+1), times[i]);
+        sum += arr->data[i];
+        printf("Run #%zu Data: Time=%fms, Speed=%f MB/s\n", i + 1, arr->data[i], 0.0); // TODO- calculate the speed
+    }
+    if (arr->size > 0)
+    {
+        avg = (sum / (double)arr->size);
     }
 
-    // if (numOfSends > 0)
-    // {
-    //     avg = (sum / (double)numOfSends);
-    // }
+    bitrate = ((fSize / 1024) / ((avg) / 1000));
+    printf("Average time: %0.3lf ms\n", avg); // TODO- calculate and print the bandwith
+}
+/**
+ *
+ */
+int main(int argc, char *argv[])
+{
+    char *FIN = "I would like to end the connection please";
+    algoritm = argv[1];
+    signal(SIGPIPE, SIG_IGN); // on linux to prevent crash on closing socket
+    DynamicArray *times_arr = malloc(sizeof(DynamicArray));
+    times_arr->size = 0;
+    times_arr->capacity = 1;
+    times_arr->data = (double *)malloc(sizeof(double));
+    printf("Starting up reciever\n");
 
-    // rate = ((fSize / 1024) / (avg / 1000));
-    /////////////////////need to add prints////////////
+    int listeningSocket = -1;
+
+    if ((listeningSocket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+        perror("socket");
+    }
+
+    // Reuse the address if the server socket on was closed and remains for 45 seconds in TIME-WAIT state till the final removal.
+    int enableReuse = 1;
+    if (setsockopt(listeningSocket, SOL_SOCKET, SO_REUSEADDR, &enableReuse, sizeof(int)) < 0)
+    {
+        perror("setsockopt");
+    }
+
+    // "sockaddr_in" is the "derived" from sockaddr structure used for IPv4 communication. For IPv6, use sockaddr_in6
+    printf("Creating the receiver socket...\n");
+    struct sockaddr_in receiverAddress;
+    memset(&receiverAddress, 0, sizeof(receiverAddress));
+
+    receiverAddress.sin_family = AF_INET;
+    receiverAddress.sin_addr.s_addr = INADDR_ANY;
+    receiverAddress.sin_port = htons(RECEIVER_PORT); // network order
+
+    // Bind the socket to the port with any IP at this port
+    if (bind(listeningSocket, (struct sockaddr *)&receiverAddress, sizeof(receiverAddress)) == -1)
+    {
+        perror("bind");
+        close(listeningSocket);
+        return -1;
+    }
+
+    // Make the socket listening; actually mother of all client sockets.
+    if (listen(listeningSocket, 500) == -1) // 500 is a Maximum size of queue connection requests
+                                            // number of concurrent connections
+    {
+        perror("listen");
+        close(listeningSocket);
+        return -1;
+    }
+
+    // Accept and incoming connection
+    printf("Waiting for incoming TCP-connections...\n");
+    struct sockaddr_in senderAddress;
+    socklen_t senderAddressLen = sizeof(senderAddress);
+    memset(&senderAddress, 0, sizeof(senderAddress));
+    senderAddressLen = sizeof(senderAddress);
+    int senderSocket = accept(listeningSocket, (struct sockaddr *)&senderAddress, &senderAddressLen);
+    if (senderSocket == -1)
+    {
+        perror("listen");
+        close(listeningSocket);
+        close(senderSocket);
+        return -1;
+    }
+    char *buff = NULL;
+    int fSize = 0;
+    int received = 0;
+    printf("Sender connected, receiving the file's size...\n");
+    getInformation(senderSocket, &fSize, sizeof(int));
+    printf("received size successfuly\n");
+    buff = malloc(fSize * sizeof(char));
+    if (buff == NULL)
+    {
+        printf("malloc failed\n");
+        exit(1);
+    }
+    clock_t begin;
+    clock_t end;
+    do
+    {
+        printf("getting the file info...\n");
+        received = 0;
+        int receivedPart;
+        begin = clock();
+        memset(buff, 0, fSize);
+        while (received != fSize && strcmp(buff, FIN) != 0)
+        {
+
+            receivedPart = getInformation(senderSocket, buff, fSize - received);
+
+            if (!receivedPart)
+            {
+                break;
+            }
+
+            if (receivedPart < 0)
+            {
+                printf("ERROR! connection is close");
+                return 1;
+            }
+
+            received += receivedPart;
+        }
+        if(strcmp(buff, FIN) == 0)
+        {
+            break;
+        }
+        end = clock();
+        printf("File transfer complete\n");
+
+        if (!receivedPart)
+        {
+            break;
+        }
+        printf("reached to line 239");
+        AddToDArr(times_arr, (double)(end - begin) / CLOCKS_PER_SEC);
+
+    } while (strcmp(buff, FIN) != 0);
+    printf("Receiver closing the connection...\n");
+    close(listeningSocket);
+    close(senderSocket);
+    print_times(times_arr, fSize);
+    free(buff);
+    free_arr(times_arr);
+    return 0;
+
+    // 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
+    // git remote set-url origin https://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github.com/Oriya-Sigawy/cn_ex3.git
+    // https://github.com/Oriya-Sigawy/cn_ex3
 }
